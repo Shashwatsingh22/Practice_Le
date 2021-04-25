19ff75f15e14e6ff93bc7240bf8f5312b6f389eb
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2021-04-26 01:54:35 +0530

    updated

--- DSA_Complete/Array/Find_Missing_Elements.cpp
+++ DSA_Complete/Array/Find_Missing_Elements.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int summ(int A[],int n)
@@ -69,7 +70,7 @@ cout<<missing_element<<endl;
    }
 }
 
-int multiple_element_missing(int A[],int n)
+int multiple_element_missing(int A[],int n)  //O(n^2)
 {
     for(int i=0;i<n;i++)
     {
@@ -80,6 +81,56 @@ int multiple_element_missing(int A[],int n)
         }
     }
     return n;
+}
+
+vector <int> merging(int A[],int n,vector <int> v,int m)
+{
+   vector <int> f;
+   int i=0,j=0;
+   while(i<n && j<m)
+   {
+       if(A[i]==v[j])
+       {
+           f.push_back(A[i]);
+           i++;
+           j++;
+       }
+       else if(A[i]<v[j])
+       {
+           f.push_back(A[i]);
+           i++;
+       }
+       else if(A[i]>v[j])
+       {
+           f.push_back(v[j]);
+           j++;
+       }
+       
+   }
+   for(;i<n;i++)
+   {
+       f.push_back(A[i]);
+   }
+   for(;j<m;j++)
+   {
+       f.push_back(v[j]);
+   }
+   return f;
+}
+
+vector <int> multiple_element_missing_O_n(int A[],int n) //We will do it in oreder of an by 
+{                                               //the help of Merging becoz it is Sorted
+    vector <int> v1;
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]-i!=A[0])
+        {
+           cout<<A[0]+i<<" ";
+           v1.push_back(A[0]+i);
+        }
+    }
+    
+    return merging(A,n,v1,v1.size());
 }
 
 void Display(int A[],int n)
@@ -94,12 +145,23 @@ void Display(int A[],int n)
 int main()
 {
     //int A[10]={1,2,3,4,5,7,8,9,10};
-    int A[11]={10,12,13,15,16,18,20};
-    int len=7;
+    //int A[11]={10,12,13,15,16,18,20};
+    int A[10]={11,13,18,20};
+    int len=4;
     Display(A,len);
     //len=find_single_element_from_the_sorted_array_of_n_natural_num(A,len,10);
     //len=find_single_element_from_natural_num_but_starting_from_random_place(A,len);
     len=multiple_element_missing(A,len);
     cout<<endl;
     Display(A,len);
+    cout<<endl;
+    int B[10]={11,13,18,20};
+    len=4;
+    vector<int> v=multiple_element_missing_O_n(B,len);
+    cout<<endl;
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
 }
--- DSA_Complete/Stack/creation_pod_stack_by_Array.cpp
+++ DSA_Complete/Stack/creation_pod_stack_by_Array.cpp
@@ -9,29 +9,68 @@ class stack
     int *A;
 };
 
-int isEmpty()
+int isEmpty(stack temp)
 {
-
+  if(temp.top==-1)
+  {
+      return 1;
+  }
+  return 0;
 }
 
-int isFull()
+int isFull(stack temp)
 {
-
+   if(temp.top==temp.size-1)
+   {
+       return 1;
+   }
+   return 0;
 }
 
-int peek()
+int peek(stack temp,int i)
 {
-
+   int pos=temp.top+i-1;
+   
+   if(pos<0)
+   {
+       cout<<"Invalid Position"<<endl;
+       return -1;
+   }
+   else
+   {
+       return temp.A[pos];
+   }
 }
 
-void push(int data)
+void push(stack *temp,int data)
 {
-
+   if(temp->top==temp->size-1)
+   {
+      cout<<"Stack OverFlow"<<endl;
+      return;
+   }
+   else
+   {
+       temp->top++;
+       temp->A[temp->top]=data;
+       return;
+   }
 }
 
-int pop()
+int pop(stack *temp)
 {
-
+    if(temp->top==-1)
+    {
+        cout<<"Stack Underflow"<<endl;
+        return -1;
+    }
+    else
+    {
+      int x=-1;
+      x=temp->A[temp->top];
+      temp->top--;
+      return x;
+    }
 }
 
 int main()
@@ -45,6 +84,38 @@ int main()
    cout<<"Choice Your Operatations : ";
    while(true)
    {
-    cout<<"1. Insert \n"<<"2. Deletion \n"<<" ";
+    cout<<"1. Insert \n"<<"2. Deletion \n"<<"3. Empty ?\n"<<"4. Full ?\n"<<"5. Give the Index to know about element.\n"<<endl;
+    int choice=0;
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        int data;
+        cin>>data;
+        push(&st,data);
+        break;
+
+    case 2:
+        cout<<pop(&st)<<" is Deleted"<<endl;
+        break;    
+    
+    case 3:
+        cout<<isEmpty(st)<<endl;
+        break;
+
+    case 4:
+        cout<<isFull(st)<<endl;
+        break;
+
+    case 5:
+        int index;
+        cin>>index;
+        cout<<peek(st,index)<<endl;
+        break;
+
+    default:
+        cout<<"Invalid Choice";
+        break;
+    }
    }
 }
