b202b74e38f28073cfb18333326c6a926aee254f
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2021-05-06 14:27:18 +0530

    updated

---
+++ GfG/LinkedList/Remove duplicate element from sorted Linked List(By Using Normal Container O(n sq).cpp
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+void removeDuplicates(struct Node* head)
+{
+    struct Node *ptr=head->next;
+    struct Node *qtr=head;
+    
+    if(ptr==NULL)
+    {
+        return;
+    }
+    
+    while(ptr!=NULL)
+    {
+       
+       if(qtr->data == ptr->data)
+       {
+           qtr->next=ptr->next;
+           free(ptr);
+           ptr=qtr->next;
+       }
+       else
+       {
+        qtr=ptr;
+        ptr=ptr->next;
+       }
+       
+    }
+    return;
+    
+}
+
+// { Driver Code Starts.
+
+    
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      removeDuplicates(start);
+      display(start);
+      printf("\n");
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+ void display(struct Node *head)
+{
+   while(head!=NULL)
+  {
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
+
+
+
+
+  // } Driver Code Ends
