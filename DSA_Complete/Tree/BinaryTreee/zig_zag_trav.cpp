6124b9239faaf022f33bac02fdf9ab734e371a36
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-01-23 14:48:33 +0530

    update

--- DSA_Complete/Tree/BinaryTreee/zig_zag_trav.cpp
+++ DSA_Complete/Tree/BinaryTreee/zig_zag_trav.cpp
@@ -60,3 +60,66 @@ temp=1;}
         return v;
     
     }
+
+
+    /* GeekForGeek */
+
+    vector <int> zigZagTraversal(Node* root)
+    {
+        int temp=1;
+        //When temp==1 then reverse that vector.
+        //when temp==0 then no need.
+        
+        queue<Node*> q;
+        vector<int> v;
+        
+        if(!root) return v;
+        
+        v.push_back(root->data);
+        q.push(root);
+        
+        while(true)
+        {
+        vector<int> t;
+           
+           int Size=q.size();
+           Node *ptr=NULL;
+           
+           if(Size==0) return v;
+           
+           while(Size>0)
+           {
+              
+              ptr=q.front();
+              q.pop();
+              
+              if(ptr->left)
+              {
+                  t.push_back(ptr->left->data);
+                  q.push(ptr->left);
+              }
+              if(ptr->right)
+              {
+                  t.push_back(ptr->right->data);
+                  q.push(ptr->right);
+              }
+              Size--;
+           }
+           
+           if(temp==1)
+           {
+               reverse(t.begin(),t.end());
+               temp=0;
+           }
+           else
+           {
+               temp=1;
+           }
+           
+           v.insert(v.end(),t.begin(),t.end());
+          
+        }
+    
+        return v;
+    	// Code here
+    }
