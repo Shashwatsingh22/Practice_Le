3d14153fd612111b814b686c123c2362ca8c7421
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-02-18 01:43:24 +0530

    update

--- DSA_Complete/Tree/BinaryTreee/isSymmetricTree.cpp
+++ DSA_Complete/Tree/BinaryTreee/isSymmetricTree.cpp
@@ -94,3 +94,47 @@ public:
         return true;
     }
 };
+
+
+//////
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    bool ans=true;
+    void trav(TreeNode* ptr, TreeNode* qtr)
+    {
+        if(!ptr && !qtr) return;
+        if(!ptr || !qtr)
+        {
+            ans=false;
+            return;
+        }
+            if(ptr->val != qtr->val) ans=false;
+        trav(ptr->left,qtr->left);
+        trav(ptr->right,qtr->right);
+    }
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        trav(p,q);
+        return ans;
+    }
+};
+
+////
+
+bool isSameTree(Node* p, Node* q)
+{
+    if(!p && !q) return q->val==p->val;
+    return (p->val,q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+}
--- DSA_Complete/Tree/BinaryTreee/verticalOT.cpp
+++ DSA_Complete/Tree/BinaryTreee/verticalOT.cpp
@@ -1,3 +1,73 @@
+//leetcode --> Saying that you need keep sorded the node data when they are at same level
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        if(!root) return ans;
+        
+        /* Map --> < INDEX < LEVEL , DATA_CONTAINER>  */
+        map<int,map<int,multiset<int>>> m;
+        /* queue --> <INDEX < LEVEL , ADDRESS >>> */
+        queue<pair<int,pair<int,TreeNode*>>> q;
+        
+        q.push({0,{0,root}});
+        int level=-1;
+        while(true)
+        {
+           int Size=q.size();
+           if(!Size) break;
+           level++;
+            while(Size>0)
+            {
+              auto qtr=q.front();
+              q.pop();
+              TreeNode* ptr=qtr.second.second;
+              int ind=qtr.first;
+            
+               m[ind][level].insert(ptr->val);
+               if(ptr->left) q.push({ind-1,{level,ptr->left}});
+               if(ptr->right) q.push({ind+1,{level,ptr->right}});
+             
+                   Size--;
+             }
+            //cout<<level<<" ";
+        }
+        //cout<<endl;
+        for(auto temp : m)
+        {
+            vector<int> tep;
+                
+            for(auto t : temp.second)
+            {
+                for(auto Ans:t.second)
+                {
+                  tep.push_back(Ans);  
+                   
+                }
+                
+            }
+            ans.push_back(tep);
+        }
+        
+        return ans;
+    }
+};
+
+
+//GFG--> Can give correct
 class Solution
 {
     public:
